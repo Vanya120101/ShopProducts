82221da9c93d9e0b95eed69144d47972eebed9d1
Ваня Смирнов <Vanya.120101@yandex.ru>
2021-02-13 06:34:47 +0300

    Создана основная инфраструктура

--- ShopProducts/Views/ShopProductsMainForm.cs
+++ ShopProducts/Views/ShopProductsMainForm.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ShopProducts
+namespace ShopProducts.Views
 {
-    public partial class Form1 : Form
+    public partial class ShopProductsMainForm : Form
     {
-        public Form1()
+        public ShopProductsMainForm()
         {
             InitializeComponent();
         }
