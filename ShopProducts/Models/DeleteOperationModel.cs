4cadbe2dc7ee45ac6e46ea9ff2a745814f9ec7c5
Ваня Смирнов <Vanya.120101@yandex.ru>
2021-02-19 05:55:44 +0300

    Реализованы операции удаления

---
+++ ShopProducts/Models/DeleteOperationModel.cs
@@ -0,0 +1,71 @@
+﻿using ShopProducts.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopProducts.Models
+{
+    class DeleteOperationModel : IDeleteOperationModel
+    {
+        private void Delete(object data)
+        {
+            SqlCommand command = data as SqlCommand;
+
+
+            DataContext.OpenConnection();
+
+            command.ExecuteNonQuery();
+
+            DataContext.CloseConnection();
+        }
+        public void DeleteOrders(object data)
+        {
+            DataRow row = data as DataRow;
+
+            string commandString = @"DELETE Orders 
+                                    WHERE OrderId = @OrderId";
+
+            SqlCommand deleteCommand = new SqlCommand(commandString, DataContext.GetConnection());
+            deleteCommand.Parameters.AddWithValue("OrderId", row["OrderId"]);
+
+
+            Delete(deleteCommand);
+            row.AcceptChanges();
+
+        }
+
+        public void DeleteProducts(object data)
+        {
+            DataRow row = data as DataRow;
+
+            string commandString = @"DELETE Products 
+                                    WHERE ProductId = @ProductId";
+
+            SqlCommand deleteCommand = new SqlCommand(commandString, DataContext.GetConnection());
+            deleteCommand.Parameters.AddWithValue("ProductId", row["ProductId"]);
+
+
+            Delete(deleteCommand);
+            row.AcceptChanges();
+        }
+
+        public void DeleteUsers(object data)
+        {
+            DataRow row = data as DataRow;
+
+            string commandString = @"DELETE Users 
+                                    WHERE UserId = @UserId";
+
+            SqlCommand deleteCommand = new SqlCommand(commandString, DataContext.GetConnection());
+            deleteCommand.Parameters.AddWithValue("UserId", row["UserId"]);
+
+
+            Delete(deleteCommand);
+            row.AcceptChanges();
+        }
+    }
+}
---
+++ ShopProducts/Models/Interfaces/IDeleteOperationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopProducts.Models.Interfaces
+{
+    interface IDeleteOperationModel
+    {
+        void DeleteUsers(object data);
+        void DeleteProducts(object data);
+        void DeleteOrders(object data);
+    }
+}
