51a0036e766a9a33a75cd3059af9ad46190f57c8
Ваня Смирнов <Vanya.120101@yandex.ru>
2021-02-19 06:15:45 +0300

    Реализована филтрация таблиц

---
+++ ShopProducts/Models/OperationWithDataBase/FilterOperationModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopProducts.Models.OperationWithDataBase
+{
+    class FilterOperationModel
+    {
+        LoadOperationModel LoadOperationModel;
+        public FilterOperationModel()
+        {
+            LoadOperationModel = new LoadOperationModel();
+        }
+
+        public object GetUsersOrders(int UserId)
+        {
+            DataView usersOrders = new DataView();
+            usersOrders.Table = LoadOperationModel.Orders as DataTable;
+            usersOrders.RowFilter = $"UserId = {UserId}";
+
+            return usersOrders;
+        }
+
+        public object GetProducts()
+        {
+            DataView products = new DataView();
+            products.Table = LoadOperationModel.Products as DataTable;
+            products.RowFilter = "Quantity > 0";
+
+            return products;
+        }
+    }
+}
